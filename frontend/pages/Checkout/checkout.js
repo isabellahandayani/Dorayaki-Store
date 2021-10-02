3daa91b68ca5276473339b51e3e008adc02816bb
isabellahandayani <13519081@std.stei.itb.ac.id>
2021-10-02 16:39:32 +0700

    finish user checkout page

--- frontend/pages/Checkout/checkout.js
+++ frontend/pages/Checkout/checkout.js
@@ -1,35 +1,35 @@
 var modal = document.getElementById("modal");
 
-var btn = document.getElementById("edit-btn");
-
 var close = document.getElementById("close-btn");
 
-btn.onclick = function() {
-  modal.style.display = "block";
+var btns = document.getElementsByClassName("edit-btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    modal.style.display = "block";
+  });
 }
 
 document.querySelector("#decr").addEventListener("click", decr);
 document.querySelector("#incr").addEventListener("click", incr);
 
-
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
 
-close.onclick = function() {
+close.onclick = function () {
   modal.style.display = "none";
-}
+};
 
-function incr(){
-  var amount = parseInt(document.getElementById('amount').innerText);
+function incr() {
+  var amount = parseInt(document.getElementById("amount").innerText);
   amount++;
   document.getElementById("amount").innerText = amount;
 }
 
-function decr(){
-  var amount = parseInt(document.getElementById('amount').innerText);
+function decr() {
+  var amount = parseInt(document.getElementById("amount").innerText);
   amount--;
-  document.getElementById("amount").innerText = amount >= 0? amount : 0;
+  document.getElementById("amount").innerText = amount >= 0 ? amount : 0;
 }
