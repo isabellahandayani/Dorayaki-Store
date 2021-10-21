76c6e46a3eeedf1a8f12858ef7b80180fe038067
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-10-21 14:59:04 +0700

    add get detail request

--- frontend/pages/detail/detail.js
+++ frontend/pages/detail/detail.js
@@ -1,8 +1,40 @@
+let xhr = new XMLHttpRequest();
+const API_URL = "http://localhost/spidermen-web/backend/api/";
+const IMG_PATH = "../../../backend/image/";
+const defaultImg = "../../assets/images/dorayaki.png";
+
+window.onload = () => {
+  var id = window.location.hash.split('#')[1];
+
+  xhr.open("GET", API_URL+`detail-dorayaki.php?id_dorayaki=${id}`);
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let res = JSON.parse(xhr.response);
+      
+      if (res.statusCode == 200) {
+        loadData(res.data);
+      }
+    }
+  }
+
+  xhr.send();
+};
+
+const loadData = (data) => {
+  const {dorayaki_name, stock, sold_stock, price, desc, photo} = data;
+
+  document.getElementById('dorayaki-name').innerHTML = dorayaki_name;
+  document.getElementById('dorayaki-img').src = photo ? IMG_PATH+photo : defaultImg;
+  document.getElementById('stock').innerHTML = `${stock} Biji`;
+  document.getElementById('price').innerHTML = `Rp. ${price}`;
+  document.getElementById('sold').innerHTML = `${sold_stock} Terjual`;
+  document.getElementById('desc').innerHTML = desc;
+}
+
 const deleteDorayaki = (url, callback, data) => {
   /*
     DELETE METHOD
   */
-  let xhr = new XMLHttpRequest();
   xhr.overrideMimeType("application/json");
   xhr.open("DELETE", url, true);
   xhr.onreadystatechange = function () {
@@ -18,7 +50,6 @@ const deleteCallback = (data) => {
   /*
     Delete Confirmation Status
   */
- console.log(data);
   let res = JSON.parse(data);
   if (res["success"]) {
     alert("Penghapusan Berhasil");
@@ -29,5 +60,9 @@ const deleteCallback = (data) => {
 
 document.querySelector(".del-btn").addEventListener("click", function () {
   // dummy
-  deleteDorayaki("http://localhost/spidermen-web/backend/api/detail.php", deleteCallback, "id=40");
+  deleteDorayaki(
+    API_URL+"detail.php",
+    deleteCallback,
+    "id=40"
+  );
 });
