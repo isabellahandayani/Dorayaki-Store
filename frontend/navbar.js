4356a6f3e6c18d60395b3786beb567bb2f20afea
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-10-22 10:49:15 +0700

    remove searchbar on several pages

--- frontend/navbar.js
+++ frontend/navbar.js
@@ -5,29 +5,33 @@ const getCookie = function (name) {
 
 const validateAdmin = () => {
   return getCookie("isAdmin") === "1";
-}
+};
 
 const goToAddVariant = () => {
-  window.location.pathname = "frontend/pages/add-variant/"
-}
+  window.location.pathname = "frontend/pages/add-variant/";
+};
 
 window.getUsername = function () {
   return getCookie("sessionID").split("-").at(-1);
-}
+};
 
 window.searchDorayaki = () => {
   const name = document.getElementById("filter").value;
-  // window.location.href = (window.location.href.includes('pages')) 
+  // window.location.href = (window.location.href.includes('pages'))
   //  (window.location.href.includes('pages')) ? `../search/?name=${name}` : `pages/search/?name=${name}`;
-  window.history.replaceState({}, document.title, "/" + `frontend/pages/search/?name=${name}`);
-  window.location.reload(false); 
+  window.history.replaceState(
+    {},
+    document.title,
+    "/" + `frontend/pages/search/?name=${name}`
+  );
+  window.location.reload(false);
 };
 
 window.logout = () => {
   deleteCookie("sessionID");
   deleteCookie("sessionEnd");
   deleteCookie("isAdmin");
-}
+};
 
 const formatMoney = (amount) => {
   let res = amount < 0 ? "-Rp " : "Rp ";
@@ -36,11 +40,11 @@ const formatMoney = (amount) => {
   return amount != 0
     ? res + amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
     : "0";
-}
+};
 
 const deleteCookie = (name) => {
   document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-}
+};
 
 function setCookie(name, value) {
   document.cookie = `${name}= ${value}; path=/`;
@@ -53,10 +57,19 @@ const setNavbar = () => {
     document.getElementById("nav-username").style.display = "none";
     document.getElementById("nav-add-item").style.display = "none";
     document.getElementById("nav-auth-button").innerHTML =
-     window.location.pathname.includes("login") ? "Register" : "Login";
+      window.location.pathname.includes("login") ? "Register" : "Login";
     document.getElementById("nav-auth-button").onclick = () => {
-      window.location.pathname = window.location.pathname.includes("login") ? "frontend/pages/register/" : "frontend/pages/login/";
+      window.location.pathname = window.location.pathname.includes("login")
+        ? "frontend/pages/register/"
+        : "frontend/pages/login/";
     };
+
+    if (
+      window.location.pathname.includes("checkout") ||
+      window.location.pathname.includes("login") ||
+      window.location.pathname.includes("register")
+    )
+      document.getElementById("nav-search").style.display = "none";
   } else {
     document.getElementById("nav-username").innerText = window.getUsername();
     document.getElementById("nav-auth-button").innerHTML = "Logout";
@@ -65,12 +78,19 @@ const setNavbar = () => {
       window.location.pathname = "frontend/pages/login/";
     };
     document.getElementById("nav-username").onclick = () => {
-      window.location.pathname = "frontend/pages/history/"
-    }
-    document.getElementById("nav-username").style.cursor = "pointer"
+      window.location.pathname = "frontend/pages/history/";
+    };
+    document.getElementById("nav-username").style.cursor = "pointer";
 
     if (!validateAdmin()) {
       document.getElementById("nav-add-item").style.display = "none";
     }
+
+    if (
+      window.location.pathname.includes("checkout") ||
+      window.location.pathname.includes("login") ||
+      window.location.pathname.includes("register")
+    )
+      document.getElementById("nav-search").style.display = "none";
   }
-}
+};
