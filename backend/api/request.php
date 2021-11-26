0879cc9776d07a45e7750a974459915581280892
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-11-26 07:59:13 +0700

    add request page

--- backend/api/request.php
+++ backend/api/request.php
@@ -6,10 +6,23 @@ $client = new SoapClient("http://localhost:1111/request?wsdl");
 // Invoke WS method (Function1) with the request params 
 $response = $client->__soapCall("getRequests", array());
 
+$db = new PDO('sqlite:../data/dorayaki.db');
+
+$query = <<<EOF
+    SELECT update_at
+    FROM dorayaki
+    ORDER BY update_at DESC
+    LIMIT 1
+EOF;
+
+$res = $db->query($query);
+$update = $res->fetch(PDO::FETCH_ASSOC);
+
 // Print WS response
 die(json_encode(array(
   "statusCode" => 200,
-  "data" => $response->return
+  "data" => $response->return,
+  "lastUpdate" => $update['update_at']
 )));
 // echo $response;
 ?>
--- frontend/pages/request/request.js
+++ frontend/pages/request/request.js
@@ -1,6 +1,7 @@
 let xhr = new XMLHttpRequest();
 
-var data = [];
+var requestData = [];
+var lastUpdate; 
 
 // TODO: add button to update stok
 window.onload = () => {
@@ -20,9 +21,11 @@ const getRequests = () => {
     if (this.readyState === 4 && this.status === 200) {
       let response = JSON.parse(this.responseText);
       if (response.statusCode === 200) {
-        data = response.data;
-        console.log(data);
-        setRequestTable(data);
+        requestData = response.data;
+        lastUpdate = response.lastUpdate;
+        
+        document.getElementById('last-update').innerHTML = "Last Updated: " + lastUpdate;
+        setRequestTable(requestData);
       }
     } else {
       console.log(this);
@@ -79,3 +82,28 @@ const setRequestTable = (data) => {
     requestsBody.appendChild(row);
   });
 };
+
+const restock = () => {
+  date = new Date(lastUpdate);
+  console.log(date);
+
+  let newStock = {};
+
+  requestData.filter((val) => {
+    return val.status === 'accepted' && (new Date(val.createdAt)).getTime() > date.getTime(); 
+  }).forEach((val) => {
+    newStock[val.idDorayaki] = val.stokAdded;
+  });
+
+  xhr.open('PUT', '../../../backend/api/stock.php');
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      let response = JSON.parse(this.response);
+      if (response.statusCode === 200) {
+        console.log(response);
+      }
+    }
+  }
+
+  newStock && xhr.send(JSON.stringify(newStock));
+}
