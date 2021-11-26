860d0bc8b112168dc1d655181ff329d5f9db66be
isabellahandayani <isabellahandayani@gmail.com>
2021-11-26 14:11:34 +0700

    fix add variant

--- backend/api/add-variant.php
+++ backend/api/add-variant.php
@@ -1,48 +1,49 @@
 <?php
 try {
-    $db = new PDO('sqlite:../data/dorayaki.db');
+	$db = new PDO('sqlite:../data/dorayaki.db');
 
-	if(isset($_GET['getDorayaki']) && $_GET['getDorayaki']) {
+	if (isset($_GET['getDorayaki']) && $_GET['getDorayaki']) {
 		$client = new SoapClient("http://localhost:1111/dorayaki?wsdl");
 		$result = $client->getDorayakis();
 		$array = json_decode(json_encode($result), true);
 		die(json_encode($array));
 	}
 
-    if (isset($_POST['nama'])) {
-        // Check if Dorayaki Exists
+	if (isset($_POST['nama'])) {
+		// Check if Dorayaki Exists
 		$query = <<<EOF
 		SELECT dorayaki_name 
 		FROM dorayaki where dorayaki_name = ?
 		EOF;
 
-        $check = $db->prepare($query);
-		$count = $check->execute(array($_POST['nama']));
+		$check = $db->prepare($query);
+		$check->execute(array($_POST['nama']));
 
-        if ($count) {
-            echo "<script>alert('Varian Sudah Ada!');window.location.href='../../frontend/pages/add-variant/'</script>";
-        } else {
-            // Save Image
-            $name = basename($_FILES['image']['name']);
-            $upload_file = '../image/' .$name;
+		$res = $check->fetch(PDO::FETCH_ASSOC);
+		if ($res) {
+			echo "<script>alert('Varian Sudah Ada!');window.location.href='../../frontend/pages/add-variant/'</script>";
+		} else {
+			// Save Image
+			$name = basename($_FILES['image']['name']);
+			$upload_file = '../image/' . $name;
 
-            move_uploaded_file($_FILES['image']['tmp_name'], $upload_file);
+			move_uploaded_file($_FILES['image']['tmp_name'], $upload_file);
 
-            $nama = $_POST['nama'];
-            // $stok = $_POST['stok'];
-            $harga = $_POST['harga'];
-            $desc = $_POST['desc'];
+			$nama = $_POST['nama'];
+			// $stok = $_POST['stok'];
+			$harga = $_POST['harga'];
+			$desc = $_POST['desc'];
 
-            // Insert
-            $query = <<<EOF
+			// Insert
+			$query = <<<EOF
                 INSERT INTO dorayaki(id_dorayaki, dorayaki_name, stock, sold_stock, price, desc, photo)
                 VALUES(NULL, '$nama', 0, 0, $harga, '$desc', '$name');
                 EOF;
-            $db->exec($query);
+			$db->exec($query);
 
-            echo "<script>alert('Varian berhasil ditambahkan!');window.location.href='../../frontend/pages/add-variant/'</script>";
-        }
-    }
+			echo "<script>alert('Varian berhasil ditambahkan!');window.location.href='../../frontend/pages/add-variant/'</script>";
+		}
+	}
 } catch (PDOException $e) {
-    print $e->getMessage();
+	print $e->getMessage();
 }
